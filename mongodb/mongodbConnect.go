1b8fc2d1ab5e2733290a27d409910f9ef09f23ab
chenbing <chenbing@fommos.com>
2022-04-25 23:02:52 +0800

    mongodb，哈希-散列表

---
+++ mongodb/mongodbConnect.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
+//todo 参考 https://www.liwenzhou.com/posts/Go/go_mongodb/
+func main() {
+	// 设置客户端连接配置
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// 连接到MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 检查连接
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+
+	//// 指定获取要操作的数据集
+	//collection := client.Database("q1mi").Collection("student")
+}
